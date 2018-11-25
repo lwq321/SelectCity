a06ded8d8d1f61edb05a5ca65942a239bcabfd02
lwq321 <825780118@qq.com>
2018-11-25 18:20:00 +0800

    删除

--- MyApp/index.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import { YellowBox } from 'react-native';
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-import Navbar from './src/view/Navbar'
-AppRegistry.registerComponent(appName, () => App);
